d1fc1cf7248bfd7635f56c3c6bc7a9102ace14e0
Luis Miranda <luyimiranda07@gmail.com>
2021-04-20 11:26:03 -0500

    Notebook for scraping ReversoContext and Leo

---
+++ main.py
@@ -0,0 +1,17 @@
+import genanki
+
+deck_name = "Deutsch"
+deck_id = 1613951331974
+
+model_name = "Goethe Vocab List (ESP)"
+model_id = 1613951331974
+
+package_name = "Deutsch.apkg"
+
+deck = genanki.Deck(
+    model_id,
+    model_name
+)
+
+
+
